b1b5910fb07e3f34af98a3ee9dbcb6dff9af7a95
Rafael Zanini <rafael.zanini2006@gmail.com>
2025-06-06 14:53:39 -0300

    amigoSecretoBase

---
+++ amigo-secreto/js/app.js
@@ -0,0 +1,46 @@
+let amigos = [];
+
+
+function adicionar(){
+    let nomeAmigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');
+    amigos.push(nomeAmigo.value);
+    if (lista.textContent == ''){
+        lista.textContent = nomeAmigo.value;
+    }
+    else{
+        lista.textContent = lista.textContent + ', ' + nomeAmigo.value;
+    }
+    nomeAmigo.value = '';
+}
+
+
+function sortear(){
+    embaralhar(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i <amigos.length; i++){ 
+        if (i == amigos.length - 1){
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->'+ amigos[0]+ '<br>'; 
+        }
+        else{
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->'+ amigos[i+1]+ '<br>'; 
+        }
+    }
+
+}
+
+
+//função pré-pronta
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+
+function reiniciar(){
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
