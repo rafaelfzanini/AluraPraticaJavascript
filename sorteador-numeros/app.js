e32e2932d5141723b7246369ea5391130aa17f28
Rafael Zanini <rafael.zanini2006@gmail.com>
2025-06-05 21:15:11 -0300

    sorteadorDeNumBotoesEInteracao

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -5,20 +5,27 @@ function sortear(){
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
     
+    
     let sorteados = []; //cria array
     let numero;         
     for (let i =0; i < quantidade; i++){
-        numero = obterNumAleatorio(de, ate);
-        sorteados.push(numero);  //puxa o valor para o array
-
+        numero = obterNumAleatorio(de, ate); //numero entre "de" e "ate"
+        while( sorteados.includes(numero)){  //verifica se num ja foi sorteado
+            numero = obterNumAleatorio(de, ate);
+        }
+        sorteados.push(numero);  //adiciona o valor para o array
     }
-    
     //alert mostra um popup   ||   ${} -> permite inserir uma variável em uma String
     //alert(`Quantidade: ${quantidade}`)
     //alert(`Do número: ${de}`)
     //alert(`Até: ${ate}`)
     
-    alert (sorteados);
+    //busca um elemento HTML
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
+    // altera o coontepudo HTML dentro do id resultado
+    
+    alterarStatusBotao();
 }
 
 
@@ -26,4 +33,29 @@ function sortear(){
 //min e max são parâmetros -> Valores passados para a função 
 function obterNumAleatorio (min, max){
     return Math.floor(Math.random() * (max-min + 1)) + min;
+}
+
+
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar'); //variavel do botão html
+     //verifica se botão está desabilitado
+    if (botao.classList.contains('container__botao-desabilitado')){ 
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }
+    else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+    // Se botão estiver habilitado, remover classe desabilitado e add classe botão
+    // Senão faz o inverso
+}
+
+    //Deixa vazio os valores das variáveis 
+function reiniciar(){
+    document.getElementById('quantidade').value = " ";
+    document.getElementById('de').value = " ";
+    document.getElementById('ate').value = " ";
+    document.getElementById('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`
+    alterarStatusBotao();
 }
